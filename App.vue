416724d10b14fafb8c943613ae3eaf11d78c5fe8
Walkline <walkline@163.com>
2021-01-27 23:36:34 +0800

    app 启动时获取 wifi 权限

--- App.vue
+++ App.vue
@@ -1,7 +1,13 @@
 <script>
+	import wifi_handler from "common/wifi_handler.js"
+	
 	export default {
 		onLaunch: function() {
 			console.log('App Launch')
+			
+			// #ifdef APP-PLUS
+			wifi_handler.grant_wifi_permission()
+			// #endif
 		},
 		onShow: function() {
 			console.log('App Show')
