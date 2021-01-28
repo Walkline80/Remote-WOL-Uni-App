18d08e51fcb77b4594d734326653069327980ddb
Walkline <walkline@163.com>
2021-01-28 15:59:47 +0800

    增加调试代码

--- pages/settings/settings.vue
+++ pages/settings/settings.vue
@@ -1,10 +1,12 @@
 <template>
 	<view>
-		
+		<button @click="button_test();">测试</button>
 	</view>
 </template>
 
 <script>
+	import wifi_handler from "../../common/wifi_handler.js"
+	
 	export default {
 		data() {
 			return {
@@ -12,7 +14,18 @@
 			}
 		},
 		methods: {
-			
+			button_test: function () {
+				// #ifdef APP-PLUS
+				// wifi_handler.is_wifi_config_exist("zysa-2.4G")
+				if (wifi_handler.connect("wol_246F289DA320")) {
+					console.log(wifi_handler.get_dhcp_info())
+					
+					uni.showToast({
+						title: "connected"
+					})
+				}
+				// #endif
+			}
 		}
 	}
 </script>
