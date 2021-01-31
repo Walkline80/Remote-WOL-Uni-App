85c7a8e7d371fb095093ab286466de43bd017b53
Walkline <walkline@163.com>
2021-01-31 23:44:33 +0800

    增加 computer_add 和 computer_detail 空白页面

---
+++ pages/computer/computer_detail.vue
@@ -0,0 +1,17 @@
+<!--
+/**
+ * The MIT License (MIT)
+ * Copyright © 2021 Walkline Wang (https://walkline.wang)
+ * 
+ * https://gitee.com/walkline/remote-wol-uni-app
+ */
+-->
+
+<template>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
