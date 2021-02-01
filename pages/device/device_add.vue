2e9a06cd262c6b1d080ffbac839151427ef9d942
Walkline <walkline@163.com>
2021-02-02 00:46:07 +0800

    测试使用下拉刷新代替按钮

--- pages/device/device_add.vue
+++ pages/device/device_add.vue
@@ -9,6 +9,9 @@
 
 <template>
 	<view>
+		<view class="page_notice" v-show="show_notice">
+			<text>下拉搜索周围的硬件设备</text>
+		</view>
 		<view>
 			<uni-list v-for="(item, index) in wifi_list" :key="index" :border="false">
 				<uni-list-item
@@ -30,58 +33,102 @@
 	export default {
 		data() {
 			return {
-				wifi_list: []
+				wifi_list: [],
+				show_notice: true
 			}
+		},
+		onShow() {
+			this.$data.show_notice = true;
 		},
 		onLoad() {
 		},
-		onNavigationBarButtonTap(e) {
-			// search button click event
-			if (e.index == 0) {
-				console.log("searching")
+		onPullDownRefresh () {
+			console.log("searching")
+			
+			// #ifdef APP-PLUS
+			this.$data.wifi_list = wifi_handler.scan_wifi()
+			// #endif
+			
+			// #ifndef APP-PLUS
+			this.$data.wifi_list = [{
+				"id": 0,
+				"ssid": "zysa-2.4G",
+				"bssid": "d0:76:e7:10:04:3b",
+				"level": -37
+			}, {
+				"id": 1,
+				"ssid": "zysa-5G",
+				"bssid": "d0:76:e7:10:04:3d",
+				"level": -38
+			}, {
+				"id": 2,
+				"ssid": "wol_246F289DA320",
+				"bssid": "24:6f:28:9d:a3:21",
+				"level": -39
+			}, {
+				"id": 3,
+				"ssid": "TP-LINK_3470",
+				"bssid": "54:a7:03:ea:34:70",
+				"level": -58
+			}]
+			// #endif
+			
+			this.$data.show_notice = false;
+			uni.stopPullDownRefresh();
+		},
+		// onNavigationBarButtonTap(e) {
+		// 	// search button click event
+		// 	if (e.index == 0) {
+		// 		console.log("searching")
 				
-				// #ifdef APP-PLUS
-				this.$data.wifi_list = wifi_handler.scan_wifi()
-				// #endif
+		// 		// #ifdef APP-PLUS
+		// 		this.$data.wifi_list = wifi_handler.scan_wifi()
+		// 		// #endif
 				
-				// #ifndef APP-PLUS
-				this.$data.wifi_list = [{
-					"id": 0,
-					"ssid": "zysa-2.4G",
-					"bssid": "d0:76:e7:10:04:3b",
-					"level": -37
-				}, {
-					"id": 1,
-					"ssid": "zysa-5G",
-					"bssid": "d0:76:e7:10:04:3d",
-					"level": -38
-				}, {
-					"id": 2,
-					"ssid": "wol_246F289DA320",
-					"bssid": "24:6f:28:9d:a3:21",
-					"level": -39
-				}, {
-					"id": 3,
-					"ssid": "TP-LINK_3470",
-					"bssid": "54:a7:03:ea:34:70",
-					"level": -58
-				}]
-				// #endif
-			}
+		// 		// #ifndef APP-PLUS
+		// 		this.$data.wifi_list = [{
+		// 			"id": 0,
+		// 			"ssid": "zysa-2.4G",
+		// 			"bssid": "d0:76:e7:10:04:3b",
+		// 			"level": -37
+		// 		}, {
+		// 			"id": 1,
+		// 			"ssid": "zysa-5G",
+		// 			"bssid": "d0:76:e7:10:04:3d",
+		// 			"level": -38
+		// 		}, {
+		// 			"id": 2,
+		// 			"ssid": "wol_246F289DA320",
+		// 			"bssid": "24:6f:28:9d:a3:21",
+		// 			"level": -39
+		// 		}, {
+		// 			"id": 3,
+		// 			"ssid": "TP-LINK_3470",
+		// 			"bssid": "54:a7:03:ea:34:70",
+		// 			"level": -58
+		// 		}]
+		// 		// #endif
+		// 	}
 			
 			// uni.showToast({
 			// 	title: "title",
 			// 	icon:"none"
 			// })
-		},
+		// },
 		methods: {
 			device_detail_click: function (item) {
+				var that = this;
+				
 				console.log("item: " + JSON.stringify(item))
+				
 				uni.navigateTo({
 					url:"../device/device_detail?item=" + encodeURIComponent(JSON.stringify(item)),
 					events:{
 						acceptDataFromOpenedPage(data) {
-							console.log(data)
+							console.log(data);
+							
+							that.$data.wifi_list = null;
+							// uni.startPullDownRefresh();
 						}
 					}
 				})
