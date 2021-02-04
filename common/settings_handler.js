567abdc0aaa8b43d10b35096e4fa15a60448f01e
Walkline <walkline@163.com>
2021-02-05 00:02:27 +0800

    app 设置参数保存/读取模块完成保存 app 设置功能，读取设置改为同步方式

--- common/settings_handler.js
+++ common/settings_handler.js
@@ -6,15 +6,61 @@
  */
 
 const settings_filename = 'remote_wol_config.json'
-const app_settings_prefix = 'remote_wol_app_'
+const app_settings_key = 'remote_wol_app_settings'
 const device_item_prefix = 'remote_wol_device_'
 
-var save_app_settings = function (data) {
+var save_app_settings = function (settings) {
+	var result = true;
 	
+	uni.setStorage({
+		key: app_settings_key,
+		data: settings,
+		success: () => {
+		},
+		fail: () => {
+			result = false;
+		}
+	});
+	
+	return result;
 };
 
 var load_app_settings = function () {
+	var settings = {};
 	
+	try {
+		const info = uni.getStorageInfoSync();
+		
+		info.keys.forEach(function (key, index) {
+			if (key === app_settings_key) {
+				const item = uni.getStorageSync(key);
+				settings = item;
+			}
+		});
+	} catch (e) {
+		console.log("load_app_setting error", e);
+	}
+
+	// uni.getStorageInfo({
+	// 	success: (result) => {
+	// 		result.keys.forEach(function (key, index) {
+	// 			if (key.startsWith(app_settings_key)) {
+	// 				uni.getStorage({
+	// 					key: key,
+	// 					success: (result) => {
+	// 						settings = result.data
+	// 					},
+	// 					fail: () => {
+	// 					}
+	// 				});
+	// 			}
+	// 		});
+	// 	},
+	// 	fail: () => {
+	// 	}
+	// });
+	
+	return settings;
 };
 
 var save_device_item = function (item) {
@@ -26,8 +72,7 @@ var save_device_item = function (item) {
 	uni.setStorage({
 		key: item.id,
 		data: item,
-		success: () => {
-		},
+		success: () => {},
 		fail: () => {
 			result = false;
 		}
@@ -39,24 +84,38 @@ var save_device_item = function (item) {
 var load_device_items = function () {
 	var items = [];
 	
-	uni.getStorageInfo({
-		success: (result) => {
-			result.keys.forEach(function (key, index) {
-				if (key.startsWith(device_item_prefix)) {
-					uni.getStorage({
-						key: key,
-						success: (result) => {
-							items.push(result.data)
-						},
-						fail: () => {
-						}
-					});
-				}
-			});
-		},
-		fail: () => {
-		}
-	});
+	try {
+		const info = uni.getStorageInfoSync();
+		
+		info.keys.forEach(function (key, index) {
+			if (key.startsWith(device_item_prefix)) {
+				const item = uni.getStorageSync(key);
+				items.push(item);
+			}
+		});
+	} catch (e) {
+		console.log("load_device_items error", e);
+	}
+	
+	// uni.getStorageInfo({
+	// 	success: (result) => {
+	// 		result.keys.forEach(function (key, index) {
+	// 			if (key.startsWith(device_item_prefix)) {
+	// 				uni.getStorage({
+	// 					key: key,
+	// 					success: (result) => {
+	// 						items.push(result.data)
+	// 						console.log(items);
+	// 					},
+	// 					fail: () => {
+	// 					}
+	// 				});
+	// 			}
+	// 		});
+	// 	},
+	// 	fail: () => {
+	// 	}
+	// });
 	
 	return items;
 };
@@ -64,10 +123,8 @@ var load_device_items = function () {
 var remove_device_item = function (key) {
 	uni.removeStorage({
 		key: key,
-		success: () => {
-		},
-		fail: () => {
-		}
+		success: () => {},
+		fail: () => {}
 	});
 }
 
