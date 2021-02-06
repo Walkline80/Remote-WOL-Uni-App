87d51e84cb3dd65c6fc1c9266af26e0c6a90a432
Walkline <walkline@163.com>
2021-02-06 15:03:54 +0800

    更换 mqtt 客户端库，增加 onUnload 事件，增加全局自定义事件监听

--- pages/computer/computer_manage.vue
+++ pages/computer/computer_manage.vue
@@ -40,25 +40,38 @@
 	</view>
 </template>
 <script>
+	import settings_handler from '../../common/settings_handler.js'
+	import mqtt from 'common/mqtt.min.js'
+	
+	var mqtt_client = null;
+	
 	export default {
 		data() {
 			return {
-				
+				app_settings: {}
 			}
 		},
 		onNavigationBarButtonTap(e) {
 			// search button click event
-			if (e.index == 0) {
+			if (e.index === 0) {
 				this.$refs.drawer.open();
 			}
-			
-			// uni.showToast({
-			// 	title: "title",
-			// 	icon:"none"
-			// })
+		},
+		onUnload() {
+			this.end_mqtt_client();
 		},
 		onLoad(options) {
-
+			this.$data.app_settings = settings_handler.load_app_settings();
+			this.start_mqtt_client();
+			
+			uni.$on('app_settings_update', () => {
+				this.$data.app_settings = settings_handler.load_app_settings();
+				this.start_mqtt_client();
+			});
+			
+			uni.$on('app_settings_validate', () => {
+				this.end_mqtt_client();
+			});
 		},
 		onReady() {
 			// #ifdef APP-PLUS
@@ -66,6 +79,20 @@
 			// #endif
 		},
 		methods: {
+			end_mqtt_client () {
+				if (mqtt_client) {mqtt_client.end({force: true});}
+			},
+			set_mqtt_indicator_status (status) {
+				var color = status ? '#F0AD4E' : '#b4b4b4';
+				
+				// #ifdef APP-PLUS
+				var webview = this.$mp.page.$getAppWebview();
+				
+				webview.setTitleNViewButtonStyle(1, {
+					color: color
+				});
+				// #endif
+			},
 			open_settings_page () {
 				// this.$refs.drawer.close();
 				
@@ -89,6 +116,68 @@
 					url: "../about/about",
 					animationType: "slide-in-right"
 				})
+			},
+			start_mqtt_client() {
+				var settings = this.$data.app_settings;
+				
+				if (Object.keys(settings).length === 0) {return;}
+				
+				this.end_mqtt_client();
+				
+				// var mqtt = require('mqtt/dist/mqtt.js');
+				var mqtt_topic = (settings.mqtt_is_bigiot ? settings.mqtt_bigiot_username : settings.mqtt_client_id) + '/remove_wol_device/#';
+				var options = {
+					keepalive: settings.mqtt_keepalive,
+					clientId: settings.mqtt_client_id,
+					username: settings.mqtt_username,
+					password: settings.mqtt_password,
+					clean: true,
+					reconnectPeriod: 1000,
+					connectTimeout: 10 * 1000,
+				};
+			
+				// #ifdef APP-PLUS
+				mqtt_client = mqtt.connect(`wx://${settings.mqtt_host}:${settings.mqtt_port}/mqtt`, options);
+				// #endif
+				
+				// #ifdef H5
+				mqtt_client = mqtt.connect(`ws://${settings.mqtt_host}:${settings.mqtt_port}/mqtt`, options);
+				// #endif
+			
+				mqtt_client.on('connect', () => {
+					console.log("connected");
+					
+					mqtt_client.subscribe(mqtt_topic, (error, granted) => {
+						if (!error) {
+							console.log('no error');
+							this.set_mqtt_indicator_status(true);
+						} else {
+							console.log('got error', error);
+						}
+					});
+				}).on('reconnect', () => {
+					console.log('reconnecting...');
+					this.set_mqtt_indicator_status(false);
+				}).on('error', (error) => {
+					console.log('on error', error);
+				}).on('message', (topic, message) => {
+					console.log(`topic: ${topic}, message: ${message.toString()}`);
+					
+					// if (topic === mqtt_topic) {
+					// 	if (message.toString() === 'success') {
+					// 		mqtt_client.end({forcus:true});
+					// 	}
+					// }
+				}).on('disconnect', () => {
+					console.log("disconnect");
+					this.set_mqtt_indicator_status(false);
+				}).on('end', () => {
+					console.log('ended');
+					this.set_mqtt_indicator_status(false);
+				}).on('close', () => {
+					console.log('closed');
+					this.set_mqtt_indicator_status(false);
+				});
 			}
 		}
 	}
