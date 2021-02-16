3f7a6f2aef8ca2727763a47fa6c953f96b6edb26
Walkline <walkline@163.com>
2021-02-16 12:30:32 +0800

    修复页面文件名错误

---
+++ pages/computer/computer_add.vue
@@ -0,0 +1,58 @@
+<!--
+/**
+ * The MIT License (MIT)
+ * Copyright © 2021 Walkline Wang (https://walkline.wang)
+ * 
+ * https://gitee.com/walkline/remote-wol-uni-app
+ */
+-->
+
+<template>
+	<view>
+		<view v-if="mode==='import'">
+			<text>import</text>
+		</view>
+		<view v-else-if="mode==='scan'">
+			<text>scan</text>
+		</view>
+	</view>
+</template>
+
+<script>
+	import test_data from '../../others/device_test_data.js'
+	import settings_handler from '../../common/settings_handler.js'
+	
+	export default {
+		data () {
+			return {
+				mode: 'input',
+			}
+		},
+		onUnload() {
+			
+		},
+		onLoad(options) {
+			this.$data.mode = options.mode || 'import'
+		},
+		onShow() {
+			
+		},
+		onHide() {
+			
+		},
+		onReady() {
+			// #ifdef APP-PLUS
+			this.$scope.$getAppWebview().evalJS('plus.android.invoke(plus.android.currentWebview(), "setForceDarkAllowed", false)')
+			// #endif
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.button-group {
+		display: flex;
+	}
+</style>
