aa603f14cfaf9088e03ce9cf63983fae65b1fc17
Walkline <walkline@163.com>
2021-02-16 12:28:34 +0800

    增加 pc 设置相关函数

--- common/settings_handler.js
+++ common/settings_handler.js
@@ -8,6 +8,7 @@
 // const settings_filename = 'remote_wol_config.json'
 const APP_SETTINGS_KEY = 'remote_wol_app_settings'
 const DEVICE_ITEM_PREFIX = 'remote_wol_device_'
+const PC_ITEM_PREFIX = 'remote_wol_pc_'
 
 /**
  * 保存 app 设置
@@ -75,6 +76,27 @@ const save_device_item = (item) => {
 }
 
 /**
+ * 保存 pc 设备设置
+ * 
+ * @param {object} item - 要保存的 pc 设备设置对象
+ * @return {boolean}
+ */
+const save_pc_item = (item) => {
+	let result = true
+	
+	item.id = PC_ITEM_PREFIX + item.mac_address
+	
+	try {
+		uni.setStorageSync(item.id, item)
+	} catch (error) {
+		console.log('save_pc_item error', error)
+		result = false
+	}
+	
+	return result
+}
+
+/**
  * 读取所有硬件设备设置
  * 
  * @return {array} 返回包含所有硬件设备设置的数组
@@ -99,6 +121,30 @@ const load_device_items = () => {
 }
 
 /**
+ * 读取所有 pc 设备设置
+ * 
+ * @return {array} 返回包含所有 pc 设备设置的数组
+ */
+const load_pc_items = () => {
+	let items = []
+	
+	try {
+		const info = uni.getStorageInfoSync()
+		
+		info.keys.forEach((key) => {
+			if (key.startsWith(PC_ITEM_PREFIX)) {
+				const item = uni.getStorageSync(key)
+				items.push(item)
+			}
+		})
+	} catch (error) {
+		console.log('load_pc_items error', error)
+	}
+
+	return items
+}
+
+/**
  * 读取指定硬件设备设置
  * 
  * @param {string} key - 要读取的硬件设备的 mac 地址（不包含 mac 地址分隔符）
@@ -113,6 +159,26 @@ const get_device_item = (key) => {
 		if (!item) {item = null}
 	} catch (error) {
 		console.log('get_device_item error', error)
+	}
+	
+	return item
+}
+
+/**
+ * 读取指定 pc 设备设置
+ * 
+ * @param {string} key - 要读取的 pc 设备的 mac 地址（包含 mac 地址分隔符）
+ * @return {object} 返回指定的 pc 设备设置
+ */
+const get_pc_item = (key) => {
+	let item = {}
+	
+	try {
+		item = uni.getStorageSync(PC_ITEM_PREFIX + key)
+
+		if (!item) {item = null}
+	} catch (error) {
+		console.log('get_pc_item error', error)
 	}
 	
 	return item
@@ -143,6 +209,30 @@ const get_device_item_counts = () => {
 }
 
 /**
+ * 读取 pc 设备设置数量
+ * 
+ * @return {int} 返回 pc 设备设置总数
+ */
+const get_pc_item_counts = () => {
+	let items = []
+	
+	try {
+		const info = uni.getStorageInfoSync()
+		
+		info.keys.forEach((key) => {
+			if (key.startsWith(PC_ITEM_PREFIX)) {
+				const item = uni.getStorageSync(key)
+				items.push(item)
+			}
+		})
+	} catch (error) {
+		console.log('load_pc_items error', error)
+	}
+
+	return items.length
+}
+
+/**
  * 更新指定硬件设备的在线状态
  * 
  * @param {string} key - 要读取的硬件设备的 mac 地址（不包含 mac 地址分隔符）
@@ -162,13 +252,26 @@ const update_device_item_status = (key, status) => {
 /**
  * 删除指定硬件设备设置
  * 
- * @param {string} key - 要读取的硬件设备的 mac 地址（不包含 mac 地址分隔符）
+ * @param {string} key - 要删除的硬件设备的 mac 地址（不包含 mac 地址分隔符）
  */
 const remove_device_item = (key) => {
 	try {
 		uni.removeStorageSync(key)
 	} catch (error) {
 		console.log('remove_device_item error', error)
+	}
+}
+
+/**
+ * 删除指定 pc 设备设置
+ * 
+ * @param {string} key - 要删除的 pc 设备的 mac 地址（包含 mac 地址分隔符）
+ */
+const remove_pc_item = (key) => {
+	try {
+		uni.removeStorageSync(key)
+	} catch (error) {
+		console.log('remove_pc_item error', error)
 	}
 }
 
@@ -217,5 +320,10 @@ export default {
 	is_app_settings_exist,
 	update_device_item_status,
 	get_device_item,
-	get_device_item_counts
+	get_device_item_counts,
+	save_pc_item,
+	load_pc_items,
+	get_pc_item,
+	get_pc_item_counts,
+	remove_pc_item
 }
