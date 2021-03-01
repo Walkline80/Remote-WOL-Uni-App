c34f20ed0b0a7df3443c136ea225568d8034b33e
Walkline <walkline@163.com>
2021-03-01 23:12:04 +0800

    新增更新记录页面

---
+++ pages/webview/changelog.vue
@@ -0,0 +1,28 @@
+<!--
+/**
+ * The MIT License (MIT)
+ * Copyright © 2021 Walkline Wang (https://walkline.wang)
+ * 
+ * https://gitee.com/walkline/remote-wol-uni-app
+ */
+-->
+
+<template>
+	<view>
+		<web-view
+			src="https://gitee.com/walkline/remote-wol-uni-app#%E6%9B%B4%E6%96%B0%E8%AE%B0%E5%BD%95" />
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {}
+		},
+		methods: {}
+	}
+</script>
+
+<style>
+
+</style>
