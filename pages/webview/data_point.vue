5c6cd472034ce62c1293ae87c9ce16ba999dddc3
Walkline <walkline@163.com>
2021-03-01 23:12:27 +0800

    原查看数据点页面文件改名

---
+++ pages/webview/data_point.vue
@@ -0,0 +1,36 @@
+<!--
+/**
+ * The MIT License (MIT)
+ * Copyright © 2021 Walkline Wang (https://walkline.wang)
+ * 
+ * https://gitee.com/walkline/remote-wol-uni-app
+ */
+-->
+
+<template>
+	<view>
+		<web-view :src="app_settings.data_point_url"></web-view>
+	</view>
+</template>
+
+<script>
+	import settings_handler from '@/common/settings_handler.js'
+	
+	export default {
+		onLoad() {
+			this.$data.app_settings = settings_handler.load_app_settings()
+		},
+		data() {
+			return {
+				app_settings: {}
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
