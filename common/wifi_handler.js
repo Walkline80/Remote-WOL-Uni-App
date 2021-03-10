55062199933c9bd4d829f2038f44b1564edba69a
Walkline <walkline@163.com>
2021-03-11 00:42:39 +0800

    格式化代码，未完成

--- common/wifi_handler.js
+++ common/wifi_handler.js
@@ -13,7 +13,7 @@ plus.android.importClass('android.net.wifi.WifiManager')
 // plus.android.importClass('android.net.wifi.WifiInfo')
 // plus.android.importClass('android.net.DhcpInfo')
 
-var PERMISSIONS = [
+const PERMISSIONS = [
 	'android.permission.ACCESS_FINE_LOCATION',
 	'android.permission.ACCESS_COARSE_LOCATION',
 	'android.permission.ACCESS_WIFI_STATE',
@@ -21,21 +21,21 @@ var PERMISSIONS = [
 	'android.permission.CHANGE_WIFI_STATE'
 ]
 
-var MainActivity = plus.android.runtimeMainActivity()
-var Context = plus.android.importClass('android.content.Context')
-var wifiManager = MainActivity.getSystemService(Context.WIFI_SERVICE)
+const MainActivity = plus.android.runtimeMainActivity(),
+	Context = plus.android.importClass('android.content.Context'),
+	wifiManager = MainActivity.getSystemService(Context.WIFI_SERVICE)
 // var WifiConfiguration = plus.android.importClass('android.net.wifi.WifiConfiguration')
 
 /**
  * 请求获取所需系统权限
  */
 function grant_wifi_permission() {
-	plus.android.requestPermissions(PERMISSIONS, function(e) {
-		if (e.granted.length > 0) {
-			console.log('permission granted', e.granted.toString())
+	plus.android.requestPermissions(PERMISSIONS, result => {
+		if (result.granted.length > 0) {
+			console.log('permission granted', result.granted.toString())
 		}
-	}, function(e) {
-		console.log(JSON.stringify(e))
+	}, error => {
+		console.error(JSON.stringify(error))
 	})
 }
 
@@ -96,11 +96,7 @@ function create_wifi_config(ssid) {
 	plus.android.invoke(bit_set, 'set', 0)
 	config.plusSetAttribute('allowedKeyManagement', bit_set)
 
-	let last_config = is_wifi_config_exist(ssid)
-
-	if (last_config) {
-		console.log('remove network ' + last_config.plusGetAttribute('SSID') + ': ' + wifiManager.removeNetwork(last_config.plusGetAttribute('networkId')))
-	}
+	remove_last_wifi_config(ssid)
 
 	return config
 }
@@ -110,8 +106,8 @@ function create_wifi_config(ssid) {
  * 
  * @param {string} ssid - 需要删除的 wifi 热点名称
  */
-function remove_last_wifi_config(ssid) {
-	let last_config = is_wifi_config_exist(ssid)
+const remove_last_wifi_config = ssid => {
+	const last_config = is_wifi_config_exist(ssid)
 	
 	if (last_config) {
 		console.log('remove network ' + last_config.plusGetAttribute('SSID') + ': ' + wifiManager.removeNetwork(last_config.plusGetAttribute('networkId')))
@@ -127,11 +123,11 @@ function remove_last_wifi_config(ssid) {
 function connect(ssid) {
 	turn_on_wifi()
 	
-	let network_id = wifiManager.addNetwork(create_wifi_config(ssid))
+	const network_id = wifiManager.addNetwork(create_wifi_config(ssid))
 	console.log('new network id', network_id)
 	
 	if (network_id !== -1) {
-		let enabled = wifiManager.enableNetwork(network_id, true)
+		const enabled = wifiManager.enableNetwork(network_id, true)
 		
 		if (enabled) {
 			sleep(1000)
@@ -164,7 +160,7 @@ function get_dhcp_info() {
 	turn_on_wifi()
 	sleep(1000)
 	
-	let info = wifiManager.getDhcpInfo()
+	const info = wifiManager.getDhcpInfo()
 	let json_result = new Object()
 
 	if (info.plusGetAttribute('ipAddress') === 0) {return json_result}
@@ -183,18 +179,18 @@ function get_dhcp_info() {
 /**
  * 获取当前设备 ip 地址（通过 getConnectionInfo）
  * 
- * @return {string} 返回获取到的 ip 地址，否则返回 null
+ * @return {String} 返回获取到的 ip 地址，否则返回 null
  */
-function get_ip_address() {
+const get_ip_address = () => {
 	turn_on_wifi()
 	sleep()
 	
-	let info = wifiManager.getConnectionInfo()
-	let state = plus.android.invoke(info, 'getSupplicantState')
-	
+	const info = wifiManager.getConnectionInfo(),
+		state = plus.android.invoke(info, 'getSupplicantState')
+
 	if (state.toString() === 'COMPLETED') {
-		let ip_address = plus.android.invoke(info, 'getIpAddress')
-		
+		const ip_address = plus.android.invoke(info, 'getIpAddress')
+
 		if (ip_address === 0) {return null}
 		
 		return ip_to_string(ip_address)
@@ -215,7 +211,7 @@ function scan_wifi() {
 	wifiManager.startScan()
 	sleep(2000)
 
-	let results = wifiManager.getScanResults()
+	const results = wifiManager.getScanResults()
 	let lists = []
 
 	for (let count = 0; count < results.size(); count++) {
@@ -256,7 +252,7 @@ function ip_to_string(ip_address) {
  * @param {number} duration - 延时时长，单位 毫秒，默认值 500
  */
 function sleep(duration=500) {
-	let time = Number(new Date())
+	const time = Number(new Date())
 	while (time + duration > Number(new Date())) {}
 }
 
